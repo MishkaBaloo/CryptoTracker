0ebcb51952d1cfb3adebdcd1cbb4d096bdf637b3
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-20 22:34:57 +0300

    Creating a Coin Model
    based on JSON response from API

--- CryptoTracker/Core/Components/CircleButton/CircleButtonAnimationView.swift
+++ CryptoTracker/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -16,7 +16,7 @@ struct CircleButtonAnimationView: View {
             .stroke(lineWidth: 5.0)
             .scale(animate ? 1.0 : 0.0)
             .opacity(animate ? 0.0 : 1.0)
-            .animation(animate ? Animation.easeOut(duration: 1.0) : .none, value: animate)
+            .animation(animate ? Animation.easeOut(duration: 0.75) : .none, value: animate)
     }
 }
 
