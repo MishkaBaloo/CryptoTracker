3e594636760edd61a7bcd850edf4a55c697bf4c4
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-20 13:42:34 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Michael on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
