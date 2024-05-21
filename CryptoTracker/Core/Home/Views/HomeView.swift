5de445a83af2ad8de8c82b91469340ef4a117083
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-21 14:15:59 +0300

    Add a dynamic List to display downloaded coins
    Add a columns to sort list and transition between moving from LivePrices to Portfolio

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -20,6 +21,22 @@ struct HomeView: View {
             // content layer
             VStack {
                 homeHeader
+                
+                columnTitles
+                
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .padding(.horizontal)
+                
+                if !showPortfolio {
+                   allCoinsList
+                        .transition(.move(edge: .leading))
+                }
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
+                
                 Spacer(minLength: 0)
             }
         }
@@ -31,6 +48,7 @@ struct HomeView: View {
         HomeView()
             .toolbar(.hidden, for: .automatic)
     })
+    .environmentObject(DeveloperPreview.instance.homeVM)
 }
 
 // MARK: EXTENSION
@@ -59,5 +77,39 @@ extension HomeView {
                 }
         }
         .padding(.horizontal)
+    }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+
+        }
     }
 }
