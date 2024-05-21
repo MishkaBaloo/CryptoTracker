edad874d9248bbba883cc8b5cb28a692cdcba92b
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-21 16:53:24 +0300

    Add a reusable Networking layer
    Create errors descriptions and clean-up the code

---
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkingManager.swift
+//  CryptoTracker
+//
+//  Created by Michael on 5/21/24.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLRespone(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLRespone(url: let url): return "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unkown error occured"
+            }
+        
+        }
+    }
+    
+    static func download(url: URL) ->  AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url)})
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+        response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLRespone(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<any Publishers.Decode<AnyPublisher<Data, any Error>, [CoinModel], JSONDecoder>.Failure>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error) :
+            print(error.localizedDescription)
+        }
+    }
+    
+}
