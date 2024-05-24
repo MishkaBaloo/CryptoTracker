e74001e00f95a62b6e336f222bb59da6a6ba6e65
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-24 18:41:33 +0300

    Download and display live market data from API
    Add formattedWithAbbreviations to downloaded data

---
+++ CryptoTracker/Services/MarketDataService.swift
@@ -0,0 +1,33 @@
+//
+//  MarketDataService.swift
+//  CryptoTracker
+//
+//  Created by Michael on 5/24/24.
+//
+
+import Foundation
+import Combine
+
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscriptions: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscriptions = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscriptions?.cancel()
+
+            })
+    }
+    
+}
